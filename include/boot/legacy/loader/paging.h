a60d167c3fac0662b279553644bac939ea688ca2
Michał Pazurek <michal10.05.pk@gmail.com>
2023-08-21 12:38:04 +0200

    Legacy loader - paging improvements

--- include/boot/legacy/loader/paging.h
+++ include/boot/legacy/loader/paging.h
@@ -7,8 +7,9 @@
 #include <boot/legacy/loader/memory.h>
 #include <boot/legacy/loader/gdt.h>
 
-#define SMALL_PAGE_SIZE 0x1000  // 4KiB
-#define BIG_PAGE_SIZE 0x200000  // 2MiB
+#define SMALL_PAGE_SIZE 0x1000      // 4KiB
+#define MEDIUM_PAGE_SIZE 0x200000   // 2MiB
+#define BIG_PAGE_SIZE 0x400000000   // 1GiB
 
 #define PAGE_ADDR(x)        (x & 0x7FFFFFFFFF000)   // The first 12bits of address are 0ed to fit the flags
 //#define PT_ENTRY_ADDR(x)    (x & 0x7FFFFFFFFF000)   // The first 12bits of address are 0ed to fit the flags
@@ -31,20 +32,44 @@
 #define PAGE_PK(x)          ((x & 0xF) << 59)       // Page's protection key
 #define PAGE_XD(x)          ((x & 1) << 63)         // Execution Disable bit
 
-// For saving space, the loader creates the bigger 2MB pages!
-// It identity maps the first 4GB (this way it ensures the VESA buffer is accessible)
+extern uint8_t useBigPages;
+
+// For saving space, the loader creates the bigger 2MB pages or 1GB pages if requested and supported!
+// It identity maps the first 4GB or 512GB of memory depending on support and requested page size
 static int IdentityPaging_Initialize() {
-    uint64_t *pd = (uint64_t*)lmalloc_a(0x4000, 0x1000);
-    for (uint64_t i = 0, p = 0; i < 0x4000 / sizeof(uint64_t); i++) {
-        pd[i] = PAGE_ADDR(p) | PAGE_PRESENT(1) | PAGE_RW(1) | PAGE_BIG(1);
-        p += BIG_PAGE_SIZE;
+    uint8_t use1Gpages = 0;
+    if (useBigPages) {
+        uint32_t edx;
+        __asm__ __volatile__ ("cpuid" : "=d"(edx) : "a"(0x80000001));
+        use1Gpages = (edx << 26) & 0x01;
+    }
+
+    uint64_t *pd = NULL;
+    if (use1Gpages == 0) {  // If we don't use 1G pages create the page directory and fill it with 2MB pages
+        pd = (uint64_t*)lmalloc_a(0x4000, 0x1000);
+
+        for (uint64_t i = 0, p = 0; i < 0x4000 / sizeof(uint64_t); i++) {
+            pd[i] = PAGE_ADDR(p) | PAGE_PRESENT(1) | PAGE_RW(1) | PAGE_BIG(1);
+            p += MEDIUM_PAGE_SIZE;
+        }
     }
 
     uint64_t *pdpt = (uint64_t*)lmalloc_a(0x1000, 0x1000);
-    pdpt[0] = PAGE_ADDR((uint64_t)(uintptr_t)pd) | PAGE_PRESENT(1) | PAGE_RW(1);
-    pdpt[1] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x1000) | PAGE_PRESENT(1) | PAGE_RW(1);
-    pdpt[2] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x2000) | PAGE_PRESENT(1) | PAGE_RW(1);
-    pdpt[3] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x3000) | PAGE_PRESENT(1) | PAGE_RW(1);
+    // If we shouldn't use 1G pages
+    if (use1Gpages == 0) {  // Fill the Page Directory Pointer Table with the appropriate page directories
+        pdpt[0] = PAGE_ADDR((uint64_t)(uintptr_t)pd) | PAGE_PRESENT(1) | PAGE_RW(1);
+        pdpt[1] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x1000) | PAGE_PRESENT(1) | PAGE_RW(1);
+        pdpt[2] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x2000) | PAGE_PRESENT(1) | PAGE_RW(1);
+        pdpt[3] = PAGE_ADDR(((uint64_t)(uintptr_t)pd) + 0x3000) | PAGE_PRESENT(1) | PAGE_RW(1);
+    }
+
+    // If we should use the 1G pages
+    if (use1Gpages == 1) {  // fill the entire Page Directory Pointer Table with them
+        for (uint64_t i = 0, p = 0; i < 0x1000 / sizeof(uint64_t); i++) {
+            pdpt[i] = PAGE_ADDR(p) | PAGE_PRESENT(1) | PAGE_RW(1) | PAGE_BIG(1);
+            p += BIG_PAGE_SIZE;
+        }
+    }
 
     uint64_t *pml4 = (uint64_t*)lmalloc_a(0x1000, 0x1000);
     pml4[0] = PAGE_ADDR((uint64_t)(uintptr_t)pdpt) | PAGE_PRESENT(1) | PAGE_RW(1);
